a5f06148640a58fe4c451ca6cdce2d9b76fc3d44
Wiktoria <wiktoriagithub@gmail.com>
2024-11-27 22:45:54 +0100

    class private matrix

---
+++ github/matrix.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <iostream>
+using namespace std;
+
+class matrix {
+private:
+	int n; //rozmiar macierzy (n x n)
+	int** data; //wskaznik na dane macierzy
+};
+
+#endif // !MATRIX_H
