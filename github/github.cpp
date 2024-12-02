c058bba7869b1d707b6d092af06e011f025fc21a
Mokkaq <gbugajska051@gmail.com>
2024-12-02 09:07:58 +0100

    małe poprawki

--- github/github.cpp
+++ github/github.cpp
@@ -1,121 +1,165 @@
-﻿// github.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+﻿/**
+ * @file github.cpp
+ * @brief Plik zawiera funkcję main, która testuje różne operacje na klasie matrix.
+ */
+
 #include "matrix.h"
 #include <iostream>
+
 using namespace std;
 
-    int main() {
-        // Test 1: Tworzenie i wyświetlenie macierzy
-        cout << "------------Test 1: Tworzenie macierzy i losowe wypelnienie------------" << endl;
-        matrix m1(3);
-        m1.losuj();
-        cout << "Macierz:" << endl;
-        cout << m1 << endl;
+/**
+ * @brief Funkcja główna programu.
+ *
+ * Testuje różne funkcje i operacje na klasie matrix, takie jak tworzenie macierzy,
+ * wstawianie wartości, operacje matematyczne i inne.
+ *
+ * @return int Zwraca 0, jeśli program zakończył się poprawnie.
+ */
+int main() {
+    /**
+     * @brief Test 1: Tworzenie i wyświetlenie macierzy.
+     *
+     * Tworzy macierz o zadanym rozmiarze, losowo wypełnia ją elementami
+     * i wyświetla wynik.
+     */
+    cout << "------------Test 1: Tworzenie macierzy i losowe wypelnienie------------" << endl;
+    matrix m1(3);
+    m1.losuj();
+    cout << "Macierz:" << endl;
+    cout << m1 << endl;
 
-        // Test 2: Wstawianie i pobieranie wartości
-        cout << "---------------Test 2: Wstawianie i pobieranie wartosci???????????-----------------" << endl;
-        m1.wstaw(0, 0, 5).wstaw(1, 1, 10).wstaw(2, 2, 15);
-        cout << "Macierz po wstawieniu wartosci:" << endl;
-        cout << m1 << endl;
-        cout << "Wartosc na (0,0): " << m1.pokaz(0, 0) << endl;
-        cout << "Wartosc na (1,1): " << m1.pokaz(1, 1) << endl << endl;
+    /**
+     * @brief Test 2: Wstawianie i pobieranie wartości.
+     *
+     * Wstawia wartości do określonych pozycji w macierzy, wyświetla wynik,
+     * a następnie pobiera te wartości i wyświetla je.
+     */
+    cout << "---------------Test 2: Wstawianie i pobieranie wartosci-----------------" << endl;
+    m1.wstaw(0, 0, 5).wstaw(1, 1, 10).wstaw(2, 2, 15);
+    cout << "Macierz po wstawieniu wartosci:" << endl;
+    cout << m1 << endl;
+    cout << "Wartosc na (0,0): " << m1.pokaz(0, 0) << endl;
+    cout << "Wartosc na (1,1): " << m1.pokaz(1, 1) << endl << endl;
 
-        // Test 3: Odwracanie macierzy
-        cout << "---------------Test 3: Odwracanie macierzy------------------------" << endl;
-        cout << "Macierz przed odwroceniem:" << endl;
-        cout << m1 << endl;
-        m1.odwroc();
-        cout << "Macierz po odwroceniu:" << endl;
-        cout << m1 << endl;
+    /**
+     * @brief Test 3: Odwracanie macierzy.
+     *
+     * Wyświetla macierz przed i po odwróceniu wierszy i kolumn.
+     */
+    cout << "---------------Test 3: Odwracanie macierzy------------------------" << endl;
+    cout << "Macierz przed odwroceniem:" << endl;
+    cout << m1 << endl;
+    m1.odwroc();
+    cout << "Macierz po odwroceniu:" << endl;
+    cout << m1 << endl;
 
-        // Test 4: Wypełnianie przekątnej
-        cout << "----------------------Test 4: Wypelnianie przekatnej------------------" << endl;
-        int diag[] = { 1, 2, 3 };
-        m1.diagonalna(diag);
-        cout << "Macierz z przekatna [1, 2, 3]:" << endl;
-        cout << m1 << endl;
+    /**
+     * @brief Test 4: Wypełnianie przekątnej.
+     *
+     * Wypełnia przekątną macierzy zadanymi wartościami i wyświetla wynik.
+     */
+    cout << "----------------------Test 4: Wypelnianie przekatnej------------------" << endl;
+    int diag[] = { 1, 2, 3 };
+    m1.diagonalna(diag);
+    cout << "Macierz z przekatna [1, 2, 3]:" << endl;
+    cout << m1 << endl;
 
-        // Test 5: Wypełnianie kolumny i wiersza
-        cout << "-----------------Test 5: Wypelnianie kolumny i wiersza-------------------" << endl;
-        int col[] = { 7, 8, 9 };
-        int row[] = { 4, 5, 6 };
-        m1.kolumna(1, col).wiersz(2, row);
-        cout << "Macierz po wypelnieniu kolumny i wiersza:" << endl;
-        cout << m1 << endl;
+    /**
+     * @brief Test 5: Wypełnianie kolumny i wiersza.
+     *
+     * Wypełnia określoną kolumnę i wiersz zadanymi wartościami, a następnie wyświetla wynik.
+     */
+    cout << "-----------------Test 5: Wypelnianie kolumny i wiersza-------------------" << endl;
+    int col[] = { 7, 8, 9 };
+    int row[] = { 4, 5, 6 };
+    m1.kolumna(1, col).wiersz(2, row);
+    cout << "Macierz po wypelnieniu kolumny i wiersza:" << endl;
+    cout << m1 << endl;
 
-        // Test 6: Wyświetlanie przekątnej, nad i pod przekątną
-        cout << "---------------Test 6: Wyswietlanie przekatnej, nad i pod przekatna---------------" << endl;
-        cout << "Macierz m1:" << endl;
-        cout << m1 << endl;
-        cout << "Przekatna macierzy: ";
-        m1.przekatna();
-        cout << "\nElementy pod przekatna macierzy:";
-        m1.pod_przekatna();
-        cout << "\nElementy nad przekatna macierzy:" << endl;
-        m1.nad_przekatna();
+    /**
+     * @brief Test 6: Wyświetlanie przekątnej, nad i pod przekątną.
+     *
+     * Wyświetla przekątną, elementy nad przekątną i pod przekątną macierzy.
+     */
+    cout << "---------------Test 6: Wyswietlanie przekatnej, nad i pod przekatna---------------" << endl;
+    cout << "Macierz m1:" << endl;
+    cout << m1 << endl;
+    cout << "Przekatna macierzy: ";
+    m1.przekatna();
+    cout << "\nElementy pod przekatna macierzy:";
+    m1.pod_przekatna();
+    cout << "\nElementy nad przekatna macierzy:" << endl;
+    m1.nad_przekatna();
 
-        // Test 7: Operacje arytmetyczne na macierzach
-        cout << "-----------------Test 7: Operacje arytmetyczne na macierzach---------------------" << endl;
-        matrix m2(3);
-        m2.losuj();
-        cout << "Macierz m1:" << endl;
-        cout << m1 << endl;
+    /**
+     * @brief Test 7: Operacje arytmetyczne na macierzach.
+     *
+     * Wykonuje operacje dodawania i mnożenia macierzy oraz operacje dodawania i mnożenia macierzy przez skalary.
+     */
+    cout << "-----------------Test 7: Operacje arytmetyczne na macierzach---------------------" << endl;
+    matrix m2(3);
+    m2.losuj();
+    cout << "Macierz m1:" << endl;
+    cout << m1 << endl;
 
-        cout << "Macierz m2:" << endl;
-        cout << m2 << endl;
-        cout << "-----------------------------------" << endl;
+    cout << "Macierz m2:" << endl;
+    cout << m2 << endl;
 
-        matrix m3 = m1 + m2;
-        cout << "Suma m1 + m2:" << endl;
-        cout << m3 << endl;
-        cout << "-----------------------------------" << endl;
+    matrix m3 = m1 + m2;
+    cout << "Suma m1 + m2:" << endl;
+    cout << m3 << endl;
 
-        matrix m4 = m1 * m2;
-        cout << "Iloczyn m1 * m2:" << endl;
-        cout << m4 << endl;
-		cout << "-----------------------------------" << endl;
+    matrix m4 = m1 * m2;
+    cout << "Iloczyn m1 * m2:" << endl;
+    cout << m4 << endl;
 
-        matrix m5 = m1 + 5;
-        cout << "Dodanie 5 do kazdego elementu m1:" << endl;
-        cout << m5 << endl;
-        cout << "-----------------------------------" << endl;
+    matrix m5 = m1 + 5;
+    cout << "Dodanie 5 do kazdego elementu m1:" << endl;
+    cout << m5 << endl;
 
-        matrix m6 = 3 * m1;
-        cout << "Pomnozenie kazdego elementu m1 przez 3:" << endl;
-        cout << m6 << endl;
+    matrix m6 = 3 * m1;
+    cout << "Pomnozenie kazdego elementu m1 przez 3:" << endl;
+    cout << m6 << endl;
 
-        // Test 8: Operatory porównania
-        cout << "------------------------Test 8: Operatory porownania------------------------" << endl;
-        cout << "Macierz m1:" << endl;
-        cout << m1 << endl;
+    /**
+     * @brief Test 8: Operatory porównania.
+     *
+     * Sprawdza równość, większość i mniejszość między macierzami.
+     */
+    cout << "------------------------Test 8: Operatory porownania------------------------" << endl;
+    cout << "Macierz m1:" << endl;
+    cout << m1 << endl;
 
-        cout << "Macierz m2:" << endl;
-        cout << m2 << endl;
+    cout << "Macierz m2:" << endl;
+    cout << m2 << endl;
 
-        cout << "Czy m1 == m2? " << (m1 == m2 ? "Tak" : "Nie") << endl;
-        cout << "Czy m1 > m2? " << (m1 > m2 ? "Tak" : "Nie") << endl;
-        cout << "Czy m1 < m2? " << (m1 < m2 ? "Tak" : "Nie") << endl << endl;
+    cout << "Czy m1 == m2? " << (m1 == m2 ? "Tak" : "Nie") << endl;
+    cout << "Czy m1 > m2? " << (m1 > m2 ? "Tak" : "Nie") << endl;
+    cout << "Czy m1 < m2? " << (m1 < m2 ? "Tak" : "Nie") << endl << endl;
 
-        // Test 9: Operatory inkrementacji i dekrementacji
-        cout << "----------------Test 9: Operatory inkrementacji i dekrementacji-------------------" << endl;
-        cout << "Macierz m1:" << endl;
-        cout << m1 << endl;
-        cout << "-----------------------------------" << endl;
+    /**
+     * @brief Test 9: Operatory inkrementacji i dekrementacji.
+     *
+     * Testuje operator zwiększania (++) i zmniejszania (--) na macierzy.
+     */
+    cout << "----------------Test 9: Operatory inkrementacji i dekrementacji-------------------" << endl;
+    m1++;
+    cout << "Macierz m1 po operatorze ++:" << endl;
+    cout << m1 << endl;
 
-        m1++;
-        cout << "Macierz m1 po operatorze ++:" << endl;
-        cout << m1 << endl;
-        cout << "-----------------------------------" << endl;
-        m1--;
+    m1--;
+    cout << "Macierz m1 po operatorze --:" << endl;
+    m1--;
+    cout << m1 << endl;
 
-        m1--;
-        cout << "Macierz m1 po operatorze --:" << endl;
-        cout << m1 << endl;
-        m1++;
+    /**
+     * @brief Test 10: Szachownica.
+     *
+     * Wyświetla macierz w formie szachownicy, gdzie pola białe mają wartość 0.
+     */
+    cout << "----------------Test 10: Szachownica----------------------------" << endl;
+    m1.szachownica();
 
-        // Test 10: Szachownica
-        cout << "----------------Test 10: Szachownica----------------------------" << endl;
-        cout << "Macierz w postaci szachownicy:" << endl;
-        m1.szachownica();
-
-        return 0;
-    }
+    return 0;
+}
--- github/matrix.h
+++ github/matrix.h
@@ -5,52 +5,263 @@
 #include <iostream>
 using namespace std;
 
+/**
+ * @class matrix
+ * @brief Klasa reprezentuj¹ca macierz kwadratow¹ i wykonuj¹ca ró¿ne operacje na niej.
+ */
 class matrix {
 private:
-	int n; //rozmiar macierzy (n x n)
-	int** data; //wskaznik na dane macierzy
+    int n; ///< Rozmiar macierzy (n x n)
+    int** data; ///< WskaŸnik na dane macierzy
+
 public:
-	matrix(); //konstruktor
-	matrix(int size); //konstruktor z parametrem
-	matrix(const matrix& m); //konstruktor kopiujacy
-	matrix(int size, int* t); //konstruktor z tablica
-	~matrix(); //destruktor
-	
-	//metody
-	matrix& alokuj(int size); //alokacja pamieci
-	matrix& wstaw(int x, int y, int wartosc); //wstawianie wartosci
-	int pokaz(int x, int y); //pobieranie wartosci
-	matrix& odwroc(); //odracanie macierzy
-	matrix& losuj(); //losowanie wype³nianie macierzy
-	matrix& losuj(int x); //losowanie wype³nianie okreslonej liczby elementow
-	matrix& diagonalna(int* t); // wype³nienie przekatnej
-	matrix& diagonalna_k(int k, int* t); // wype³nienie przek¹tnej przesuniêtej o k
-	matrix& kolumna(int x, int* t); // wype³nienie kolumny
-	matrix& wiersz(int y, int* t); // wype³nienie wiersza
-	matrix& przekatna(); // pobieranie g³ównej przek¹tnej
-	matrix& pod_przekatna(); // elementy pod przek¹tn¹
-	matrix& nad_przekatna(); // elementy nad przek¹tn¹
-	matrix& szachownica(); // szachownica
+    /**
+     * @brief Konstruktor domyœlny.
+     */
+    matrix();
 
-	//operatory
-	matrix& operator+(matrix& m);
-	matrix& operator*(matrix& m);
-	matrix& operator+(int a);
-	matrix& operator*(int a);
-	matrix& operator-(int a);
-	friend matrix operator+(int a, matrix& m);
-	friend matrix operator*(int a, matrix& m);
-	friend matrix operator-(int a, matrix& m);
-	matrix& operator++(int);
-	matrix& operator--(int);
-	matrix& operator+=(int a);
-	matrix& operator-=(int a);
-	matrix& operator*=(int a);
-	bool operator==(const matrix& m) const;
-	bool operator>(const matrix& m) const;
-	bool operator<(const matrix& m) const;
+    /**
+     * @brief Konstruktor parametryczny.
+     * @param size Rozmiar macierzy.
+     */
+    matrix(int size);
 
-	// Wypisywanie
-	friend ostream& operator<<(ostream& o, const matrix& m);
+    /**
+     * @brief Konstruktor kopiuj¹cy.
+     * @param m Macierz do skopiowania.
+     */
+    matrix(const matrix& m);
+
+    /**
+     * @brief Konstruktor z tablic¹.
+     * @param size Rozmiar macierzy.
+     * @param t WskaŸnik na tablicê.
+     */
+    matrix(int size, int* t);
+
+    /**
+     * @brief Destruktor.
+     */
+    ~matrix();
+
+    /**
+     * @brief Alokuje pamiêæ dla macierzy.
+     * @param size Rozmiar macierzy.
+     * @return Referencja do macierzy.
+     */
+    matrix& alokuj(int size);
+
+    /**
+     * @brief Wstawia wartoœæ do macierzy.
+     * @param x Indeks wiersza.
+     * @param y Indeks kolumny.
+     * @param wartosc Wartoœæ do wstawienia.
+     * @return Referencja do macierzy.
+     */
+    matrix& wstaw(int x, int y, int wartosc);
+
+    /**
+     * @brief Pobiera wartoœæ z macierzy.
+     * @param x Indeks wiersza.
+     * @param y Indeks kolumny.
+     * @return Wartoœæ na okreœlonej pozycji.
+     */
+    int pokaz(int x, int y);
+
+    /**
+     * @brief Transponuje macierz.
+     * @return Referencja do macierzy.
+     */
+    matrix& odwroc();
+
+    /**
+     * @brief Wype³nia macierz losowymi wartoœciami.
+     * @return Referencja do macierzy.
+     */
+    matrix& losuj();
+
+    /**
+     * @brief Wype³nia okreœlon¹ liczbê elementów w macierzy losowymi wartoœciami.
+     * @param x Liczba elementów do wype³nienia.
+     * @return Referencja do macierzy.
+     */
+    matrix& losuj(int x);
+
+    /**
+     * @brief Wype³nia g³ówn¹ przek¹tn¹ macierzy wartoœciami z tablicy.
+     * @param t WskaŸnik na tablicê.
+     * @return Referencja do macierzy.
+     */
+    matrix& diagonalna(int* t);
+
+    /**
+     * @brief Wype³nia przek¹tn¹ przesuniêt¹ o k wartoœciami z tablicy.
+     * @param k Przesuniêcie przek¹tnej.
+     * @param t WskaŸnik na tablicê.
+     * @return Referencja do macierzy.
+     */
+    matrix& diagonalna_k(int k, int* t);
+
+    /**
+     * @brief Wype³nia kolumnê macierzy wartoœciami z tablicy.
+     * @param x Indeks kolumny.
+     * @param t WskaŸnik na tablicê.
+     * @return Referencja do macierzy.
+     */
+    matrix& kolumna(int x, int* t);
+
+    /**
+     * @brief Wype³nia wiersz macierzy wartoœciami z tablicy.
+     * @param y Indeks wiersza.
+     * @param t WskaŸnik na tablicê.
+     * @return Referencja do macierzy.
+     */
+    matrix& wiersz(int y, int* t);
+
+    /**
+     * @brief Wypisuje g³ówn¹ przek¹tn¹ macierzy.
+     * @return Referencja do macierzy.
+     */
+    matrix& przekatna();
+
+    /**
+     * @brief Wypisuje elementy pod g³ówn¹ przek¹tn¹ macierzy.
+     * @return Referencja do macierzy.
+     */
+    matrix& pod_przekatna();
+
+    /**
+     * @brief Wypisuje elementy nad g³ówn¹ przek¹tn¹ macierzy.
+     * @return Referencja do macierzy.
+     */
+    matrix& nad_przekatna();
+
+    /**
+     * @brief Wypisuje macierz w uk³adzie szachownicy.
+     * @return Referencja do macierzy.
+     */
+    matrix& szachownica();
+
+    /**
+     * @brief Dodaje dwie macierze.
+     * @param m Macierz do dodania.
+     * @return Referencja do wynikowej macierzy.
+     */
+    matrix& operator+(matrix& m);
+
+    /**
+     * @brief Mno¿y dwie macierze.
+     * @param m Macierz do mno¿enia.
+     * @return Referencja do wynikowej macierzy.
+     */
+    matrix& operator*(matrix& m);
+
+    /**
+     * @brief Dodaje skalar do macierzy.
+     * @param a Skalar do dodania.
+     * @return Referencja do wynikowej macierzy.
+     */
+    matrix& operator+(int a);
+
+    /**
+     * @brief Mno¿y macierz przez skalar.
+     * @param a Skalar do mno¿enia.
+     * @return Referencja do wynikowej macierzy.
+     */
+    matrix& operator*(int a);
+
+    /**
+     * @brief Odejmuje skalar od macierzy.
+     * @param a Skalar do odjêcia.
+     * @return Referencja do wynikowej macierzy.
+     */
+    matrix& operator-(int a);
+
+    /**
+     * @brief Dodaje skalar do macierzy.
+     * @param a Skalar do dodania.
+     * @param m Macierz do dodania.
+     * @return Wynikowa macierz.
+     */
+    friend matrix operator+(int a, matrix& m);
+
+    /**
+     * @brief Mno¿y macierz przez skalar.
+     * @param a Skalar do mno¿enia.
+     * @param m Macierz do mno¿enia.
+     * @return Wynikowa macierz.
+     */
+    friend matrix operator*(int a, matrix& m);
+
+    /**
+     * @brief Odejmuje skalar od macierzy.
+     * @param a Skalar do odjêcia.
+     * @param m Macierz do odjêcia.
+     * @return Wynikowa macierz.
+     */
+    friend matrix operator-(int a, matrix& m);
+
+    /**
+     * @brief Inkrementuje wszystkie elementy macierzy o 1.
+     * @return Referencja do macierzy.
+     */
+    matrix& operator++(int);
+
+    /**
+     * @brief Dekrementuje wszystkie elementy macierzy o 1.
+     * @return Referencja do macierzy.
+     */
+    matrix& operator--(int);
+
+    /**
+     * @brief Dodaje skalar do macierzy.
+     * @param a Skalar do dodania.
+     * @return Referencja do macierzy.
+     */
+    matrix& operator+=(int a);
+
+    /**
+     * @brief Odejmuje skalar od macierzy.
+     * @param a Skalar do odjêcia.
+     * @return Referencja do macierzy.
+     */
+    matrix& operator-=(int a);
+
+    /**
+     * @brief Mno¿y macierz przez skalar.
+     * @param a Skalar do mno¿enia.
+     * @return Referencja do macierzy.
+     */
+    matrix& operator*=(int a);
+
+    /**
+     * @brief Porównuje dwie macierze pod k¹tem równoœci.
+     * @param m Macierz do porównania.
+     * @return True, jeœli macierze s¹ równe, w przeciwnym razie false.
+     */
+    bool operator==(const matrix& m) const;
+
+    /**
+     * @brief Porównuje, czy ta macierz jest wiêksza od innej macierzy.
+     * @param m Macierz do porównania.
+     * @return True, jeœli ta macierz jest wiêksza, w przeciwnym razie false.
+     */
+    bool operator>(const matrix& m) const;
+
+    /**
+     * @brief Porównuje, czy ta macierz jest mniejsza od innej macierzy.
+     * @param m Macierz do porównania.
+     * @return True, jeœli ta macierz jest mniejsza, w przeciwnym razie false.
+     */
+    bool operator<(const matrix& m) const;
+
+    /**
+     * @brief Wypisuje macierz do strumienia wyjœciowego.
+     * @param o Strumieñ wyjœciowy.
+     * @param m Macierz do wypisania.
+     * @return Strumieñ wyjœciowy.
+     */
+    friend ostream& operator<<(ostream& o, const matrix& m);
 };
+
 #endif // !MATRIX_H
